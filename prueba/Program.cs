31468fcd6abb0758a2402c9f7389ad818b360601
Javier <javmoreno766@gmail.com>
2022-10-10 13:24:54 +0200

    Agregar archivos de proyecto.

---
+++ prueba/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prueba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca un valor para un radio");
+            int numero = int.Parse(Console.ReadLine());
+
+            double resultado = Math.PI * Math.Pow(numero, 2);
+            Console.WriteLine($"El area del circulo es: {resultado}");
+        }
+    }
+}
