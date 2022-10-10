3457aba53b6bcf16f35af2d24a1d3d419ac1cc7a
Javier <javmoreno766@gmail.com>
2022-10-10 13:36:09 +0200

    refactor: add some regions

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -10,11 +10,21 @@ namespace prueba
     {
         static void Main(string[] args)
         {
+            Practica1();   
+        }
+        #region Practica1
+        static void Practica1()
+        {
             Console.WriteLine("Introduzca un valor para un radio");
             int numero = int.Parse(Console.ReadLine());
 
             double resultado = Math.PI * Math.Pow(numero, 2);
             Console.WriteLine($"El area del circulo es: {resultado}");
         }
+        #endregion
+
+        #region Practica 2
+
+        #endregion
     }
 }
